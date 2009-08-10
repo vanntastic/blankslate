96bb3383edb5d5b5f65801498e0bece8283d959a
vann <vann@innerfusion.net>
2009-08-10 11:00:41 -0500

    updated the source and added in the rake task

--- lib/blankslate.rb
+++ lib/blankslate.rb
@@ -1,1 +1,60 @@
-# Blankslate
+module Blankslate
+  
+   # a method for only outputting a data when a condition is true
+   # useful for inserting blank slates without conditionals
+   # EX: instead of doing the following
+   # if @users.blank?
+   #     <p>There are no users</p>
+   # else
+   #     <p>There will be users here</p>
+   # end
+   # YOU CAN DO THIS :
+   # output_data_unless @users.blank? do
+   #   <p>There will be users here</p>    
+   # end
+   # you can also set it to output nothing by passing :output => :none :
+   # output_data_unless @users.blank?, :output => :none do
+   # -- your output here
+   # end
+   # isn't that more easier to read?
+   
+   # Customize the [default] output for the blank_slate by creating a partial at : 
+   # 
+   #   "app/views/blank_slates/#{name_of_controller}/#{name_of_action}"
+   # 
+   # You can use the rake task to generate a new blank slate:
+   # 
+   #   # generating a blank slate for users/index
+   #   rake blank_slates:generate FOR=users/index
+   # 
+   # OR just pass the :message option :
+   # 
+   #    output_data_unless @users.blank?, :message => "No people found..."
+   
+   def output_data_unless(conditions,options={},&block)
+     options[:message] ||= "There are no records found..."
+     
+     if options[:partial].nil?
+       blank_slate_root = File.join(RAILS_ROOT, "app", "views", "blank_slates")
+       blank_slate_path = File.join(blank_slate_root, params[:controller], 
+                          "_#{params[:action]}.erb")
+       partial_path = File.join("blank_slates", params[:controller], params[:action])
+       blank_slate_exists = File.exists?(blank_slate_path)
+       blank_slate = blank_slate_exists ? render(:partial => partial_path) : options[:message]
+     else
+       partial_ary = File.split options[:partial]
+       file, dir = "_#{partial_ary.last}.erb", partial_ary.first
+       partial_exists = File.exists? File.join(Rails.root, "app", "views", dir, file)
+       blank_slate = partial_exists ? render(:partial => options[:partial]) : options[:message]
+     end
+     
+     # rails 2.1 and after don't like the block.binding business
+     v_pre_2_1 = "concat(blank_slate, block.binding)"
+     v_after_2_1 = "concat(blank_slate)"
+     
+     output = RAILS_GEM_VERSION.to_f > 2.1 ? v_after_2_1 : v_pre_2_1
+     conditions ? (eval(output) unless options[:output] == :none) : yield
+   end
+  
+	 alias output_unless :output_data_unless
+end
--- tasks/blankslate_tasks.rake
+++ tasks/blankslate_tasks.rake
@@ -1,4 +1,28 @@
-# desc "Explaining what the task does"
-# task :blankslate do
-#   # Task goes here
-# end
+namespace :blank_slates do
+  
+  desc 'Generate a blank slate for a controller and a view'
+  task :generate do
+    if ENV['FOR'].nil?
+      puts "USAGE : rake blank_slates:generate FOR='path/to/blank/slate'"
+      exit
+    else
+      blank_slate_ary = File.split ENV['FOR']
+      file = blank_slate_ary.last
+      
+      blank_slate_path = File.join(Rails.root, "app", "views", "blank_slates")
+      Dir.mkdir blank_slate_path unless File.exists? blank_slate_path
+      
+      dir_path = File.join(blank_slate_path, blank_slate_ary.first)
+      FileUtils.mkdir_p dir_path unless File.exists? dir_path
+      
+      file_path = File.join(dir_path, "_#{file}.erb")
+      File.open(file_path,"w+") do |file|
+        file << "<!-- Here goes the beginning of a great blank slate -->"
+      end
+      
+      puts "Your blank slate has been generated at : #{file_path}"
+      system("mate #{file_path}") if system("mate")
+    end
+  end
+  
+end
